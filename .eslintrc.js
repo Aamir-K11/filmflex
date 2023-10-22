41cf20fb5ae1d9ddb79a8cf5d4a39a457de6175c
Aamir-K11 <amirkhan11999@gmail.com>
2023-10-23 00:06:02 +0500

    feat:prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,24 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb',
+  extends: ["airbnb", "eslint:recommended", "plugin:prettier/recommended"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
   },
 };
